538f6ae51d10c990b42886b670d803727d9e5766
Matt Snyder <matt.snyder@yahoo.com>
2024-07-06 23:42:34 -0400

    firebase Init

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Fragment } from 'react';
-import NavBar from './navBar';
+import NavBar from './NavBar';
 import WeatherBackdrop from './WeatherBackdrop';
 import './WeatherBackdrop.css';
+import { auth, app } from './firebaseConfig';
+
 function App() {
   return (
     <Router>
       <Fragment>
-      <WeatherBackdrop />
+        <WeatherBackdrop />
         <div className="App">
-          <NavBar></NavBar>
+          <NavBar />
         </div>
       </Fragment>
     </Router>
-
   );
 }
 
